71c8f69b8bf01db405bb25e643d56774c2bb9ddc
Matt Draghi <mtd5264@gmail.com>
2022-01-20 21:46:44 -0500

    commit tree f22a70401aeb3ed841a78498676b506a7c65363d

--- main.js
+++ main.js
@@ -3,9 +3,31 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const heartEmojis = document.querySelectorAll(`span.like-glyph`)
+heartEmojis.forEach(heartEmoji => {heartEmoji.addEventListener('click', (e) => handleLike(e))})
 
-
-
+function handleLike(e) {
+  //console.log(e.target.innerText)
+  if(e.target.innerText === EMPTY_HEART){
+    fetch(mimicServerCall())
+    .then(response => response.json())
+    .then(object => console.log(object))  //also not working????
+    // .then(() => { //what goes here?
+    //   e.target.innerText = FULL_HEART
+    //   e.target.setAttribute('class', 'activated-heart')
+    // })
+    .catch((error) => {
+      const errorModal = document.getElementById('modal')
+      errorModal.removeAttribute('class')
+      errorModal.querySelector('#modal-message').innerText = error.message
+      setTimeout(() => {errorModal.setAttribute('class','hidden')}, 3000)
+    })
+  }
+  else if(e.target.innerText === FULL_HEART){
+    e.target.innerText = EMPTY_HEART
+    e.target.removeAttribute('class')
+  }
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
